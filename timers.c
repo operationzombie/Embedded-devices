796c9b72f20d93c279e6e34a29154cf8cef1cea1
Layne Small <laynecsmall@gmail.com>
2016-07-17 19:57:46 +1200

    ready for calibration method to be written

--- timers.c
+++ timers.c
@@ -2,6 +2,9 @@
 #include "timers.h"
 #include "uart.h"
 
+#include <stdio.h>
+
+int tmr_count;
 
 /* 
  * ===  FUNCTION  ======================================================================
@@ -11,9 +14,7 @@
  * =====================================================================================
  */
 ISR(TIMER2_OVF_vect){
-  //do regular thinger when overflow
-  //
-  USART_putstring("1 second\r\n");
+  //do async timeout things
 }
 
 /* 
@@ -39,10 +40,21 @@ void TIMERS_init_async_timer(void){
   TIMSK |= (1 << TOIE2);                        /* enable overflow interrupt */
 /*TIMSK |= (1 << OCIE2);                        /1* enable output compare interrupt *1/ */
 
-  /* TCCR2 |= (1 << CS22) | (1 << CS20);           /1* set prescaler to 128, so overflow 1/second *1/ */
-  /* TCCR2 |= (1 << CS22)                          /1* set prescaler to 64, so overflow 2/second *1/ */
-  /* TCCR2 |= (1 << CS20)                          /1* prescaler 1 = 7.8125ms *1/ */
-  TIMERS_start_async_timer(5);
+  tmr_count = 0;
+  TIMERS_start_async_timer(5);                  /* set the overflow time at 1s */
+  /* TIMERS_start_io_timer(1); */
+}
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  TIMERS_set_async_compare_value
+ *  Description:  Set the async counter compare value
+ *                when the timer count is equal to this value, fire the compare 
+ *                interrupt TIMER2_COMP 
+ * =====================================================================================
+ */
+void TIMERS_set_async_compare_value(char comp){
+  OCR2 = comp;
 }
 
 
@@ -53,11 +65,12 @@ void TIMERS_init_async_timer(void){
  *                bits passed to the param timer. 
  *
  *                number prescale overflow time
+ *                0      off
  *                1      1/1      7.8125ms
  *                2      1/8      62.5ms
  *                3      1/32     250ms
  *                4      1/64     500ms
- *                5      1/128    1s
+ *                5      1/128    1m
  *                6      1/256    2s
  *                7      1/1024   8s
  *
@@ -77,12 +90,50 @@ void TIMERS_init_io_timer(void){
 
 }
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  TIMERS_start_io_timer
+ *  Description:  Like start_async_timer this starts the timer with the timer control
+ *                reg set to the given paramater. the prescaler is set by the lower 
+ *                three bits
+ *
+ *                number  prescale
+ *                0       off
+ *                1       1/1
+ *                2       1/8
+ *                3       1/64
+ *                4       1/256
+ *                5       1/1024
+ *                6       clock on T1 pin, falling edge
+ *                7       clock on T1 pin, rising edge
+ *
+ * =====================================================================================
+ */
 void TIMERS_start_io_timer(char timer){
+  TCNT1 = 0;
+  TCCR1B = timer;
+}
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  TIMERS_stop_io_timer
+ *  Description:  Stops the IO timer by setting TCCR1B = 0
+ * =====================================================================================
+ */
+void TIMERS_stop_io_timer(void){
+  TCCR1B = 0;
 
 }
 
-void TIMERS_stop_io_timer(void){
-
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  TIMERS_get_io_count
+ *  Description:  returns the current count from the io timer
+ *                by returning the value of TCNT1
+ * =====================================================================================
+ */
+int TIMERS_get_io_count(void){
+  return TCNT1;
 }
 
 /*
--- timers.h
+++ timers.h
@@ -8,5 +8,6 @@ void TIMERS_stop_async_timer(void);
 void TIMERS_init_io_timer(void);
 void TIMERS_start_io_timer(char timer);
 void TIMERS_stop_io_timer(void);
+int  TIMERS_get_io_count(void);
 
 #endif
