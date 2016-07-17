72f5fc796810843d167c9adc9dfb2e15d53b024c
Layne Small <laynecsmall@gmail.com>
2016-07-17 14:06:36 +1200

    added full function comments for uart

--- controller.c
+++ controller.c
@@ -5,23 +5,23 @@ int i;
 
 int  main()
 {
-  sei(); //enable global interrupts
+  sei();                                        //enable global interrupts
   
   //define LED outputs for blinking
   DDRA = 0x01;
   PORTA = 0x01;
 
-  USART_init(); //init usart
+  USART_init();                                 //init usart
   while(1){
-    if (USART_need_rx_update()){ //a string has been recieved, update the things
+    if (USART_need_rx_update()){                //a string has been recieved, update the things
       USART_unset_rxb_update();
       USART_copy_rxb();
 
       //print the recieved message
-      USART_putstring(USART_get_last_message());
+      USART_putstring(USART_get_last_message()); /* transmit the last recieved message (uart echo) */
 
       //delay while tx completes to print newline
-      //WARNING: this is blocking, for testing only
+      //WARNING: this is blocking, for testing onlm
       //must be removed when implementation of 
       //timing sensitive content in main loop added
       while (USART_get_tx_status()){
@@ -32,8 +32,7 @@ int  main()
     }
 
     _delay_ms(1000);
-    //toggle LED 
-    PORTA ^= 0xFF;
+    PORTA ^= 0xFF;                              /* toggle LED */
   }
 
 }
--- uart.c
+++ uart.c
@@ -1,16 +1,20 @@
 #include "global.h"
 #include "uart.h"
 
-char rx_buffer[INPUT_BUFFER_SIZE] = {'\0'}; //define recieve buffer
-char complete_buffer[INPUT_BUFFER_SIZE] ; //define buffer for finished message
+char rx_buffer[INPUT_BUFFER_SIZE] = {'\0'};     //define recieve buffer
+char complete_buffer[INPUT_BUFFER_SIZE] ;       //define buffer for finished message
 char *current_rxb;
 char *current_txb;
 char update_rxb = 0;
 char currently_tx = 0;
-int u;
+int u;                                          //uart for loop counter
 
-//define interrupt routine
-//recieve complete
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  USART_RXC_vect
+ *  Description:  the interrupt routine fired when a character has been recieved
+ * =====================================================================================
+ */
 ISR(USART_RXC_vect)
 {
   //get the recieved character from the buffer
@@ -24,7 +28,12 @@ ISR(USART_RXC_vect)
 
 }
 
-//transmit complete
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  USART_UDRE_vect
+ *  Description:  the interrupt routine fired when a character has finished tx
+ * =====================================================================================
+ */
 ISR(USART_UDRE_vect)
 {
   //if we've got a valid character in the output buffer, send it
@@ -37,7 +46,13 @@ ISR(USART_UDRE_vect)
     currently_tx = 0;
   }
 }
-/* Initialize USART */
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  USART_init
+ *  Description:  set all registers and initialize relevant variables to enable uart
+ * =====================================================================================
+ */
 void USART_init(void){
   current_rxb = rx_buffer; //set rx buffer pointer
 	
@@ -49,27 +64,61 @@ void USART_init(void){
 	UCSRC = (1<<UCSZ0)|(1<<UCSZ1)|(1<<URSEL); //set up UaRtControlandStatusRegisterC
 }
  
-/* Send string */
-//WARNING: if this is called when currently transmitting,
-//the current message pointer will be replaced with this one
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  USART_putstring
+ *  Description:  transmits a string via serial
+ *
+ *                Warning: if this is called when currently transmitting, the current 
+ *                message pointer will be replkaced with this one, meaning the 
+ *                message will not be properly transmitted
+ * =====================================================================================
+ */
 void USART_putstring(char* S){
   current_txb = S;
   UCSRB |= (1<< UDRIE);
   currently_tx = 1;
 }
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  USART_get_tx_status
+ *  Description:  getter function, reporting current status of transmission
+ * =====================================================================================
+ */
 char USART_get_tx_status(void){
   return currently_tx;
 }
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  USART_need_rx_update
+ *  Description:  getter function, reporting the current need for string recieve 
+ *                handling
+ * =====================================================================================
+ */
 char USART_need_rx_update(void){
   return update_rxb;
 }
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  USART_unset_rxb_update
+ *  Description:  setter, unsets the need_rxb_update flag, indicating the recent rxb 
+ *                action has been performed
+ * =====================================================================================
+ */
 void USART_unset_rxb_update(void){
   update_rxb = 0;
 }
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  USART_copy_rxb
+ *  Description:  copy the rx buffer to complete_buffer, useful if a buffer fills, or
+ *                transmission completes, so prepare for new transmission
+ * =====================================================================================
+ */
 void USART_copy_rxb(void){
   //copy the rx buffer, then zero it for incoming transmission
   for (u = 0; u < INPUT_BUFFER_SIZE; u++){
@@ -79,6 +128,14 @@ void USART_copy_rxb(void){
   current_rxb = rx_buffer; //reset the buffer pointer
 }
 
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  USART_get_last_message
+ *  Description:  getter, returning the last complete message recieved (or the rx_buffer
+ *                when it filled)
+ * =====================================================================================
+ */
 char* USART_get_last_message(void){
   return complete_buffer;
 }
