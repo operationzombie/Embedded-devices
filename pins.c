db71f4f1bef35cac9c573021f72bdf049a212ad4
Layne Small <laynecsmall@gmail.com>
2016-07-20 08:30:44 +1200

    added comments

--- pins.c
+++ pins.c
@@ -1,7 +1,13 @@
 #include "global.h"
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  PINS_init
+ *  Description:  Sets the input/output mode for all ports, based on the eletrical 
+ *                schematic
+ * =====================================================================================
+ */
 void PINS_init(void){
-  //set input/output modes for pins
   DDRA = 0x80;                                  /* 0b10000000 */
   DDRB = 0x70;                                  /* 0b01110000 */
   DDRC = 0x3F;                                  /* 0b00111111 */
