c26bea94b4f66777b67b7b12a93c72a7c140a282
Layne Small <laynecsmall@gmail.com>
2016-10-02 21:19:37 +1300

    ready for integration

--- controller.c
+++ controller.c
@@ -3,16 +3,51 @@
 #include "pwm.h"
 #include "pins.h"
 #include "motor.h"
+#include "stdio.h"
  
 int i;
+int speed;
+int dir;
 
+char buf[INPUT_BUFFER_SIZE];
+
+  /*
+  |===========================|
+  |          pinmap           |
+  |===========================|
+  | pin  | port |   function  |
+  | 0    | PD0  |   RX        |
+  | 1    | PD1  |   TX        |
+  | 2    | PD2  |   Int0      |
+  | 3    | PD3  |   Int1      |
+  | 7    | PD7  |   INA       |
+  | 8    | PB0  |   INB       |
+  | 9    | PB1  |   PWM1      |
+  | 10   | PB2  |   PWM2      |
+  | 13   | PB5  |   LED       |
+  |===========================|
+  */
+  
 int  main()
 {
   sei();                                        //enable global interrupts
-  
-  PINS_init();
-  MOTOR_init();
+
+  DDRB = 0x00;
+  DDRC = 0x00;
+  DDRD = 0x00; 
+  setPin(DDRB, 1,1); //pin 8  - INB
+  setPin(DDRB, 5,1); //pin 13 - LED
+  setPin(DDRD, 7, 1); //pin 7 - INA
+
+  MOTOR_init();                                 //set up motors ready to go
   USART_init();                                 //init usart
+  USART_unset_rxb_update();                     //ensure we don't end up in rx_update loop
+
+  //INTS_init_0(2);                               //enable interrupt 0, on change
+  //INTS_init_1(2);                               //enable interrupt 1, on change
+
+  USART_putstring("Initializing\r\n");
+  _delay_ms(20);
 
   while(1){
     if (USART_need_rx_update()){                //a string has been recieved, update the things
@@ -21,6 +56,7 @@ int  main()
 
       //print the recieved message
       USART_putstring(USART_get_last_message()); /* transmit the last recieved message (uart echo) */
+
 
       //delay while tx completes to print newline
       //WARNING: this is blocking, for testing onlm
@@ -33,12 +69,16 @@ int  main()
       USART_putstring("\r\n");
     }
 
-    _delay_ms(1000);
     //values are 0-25 for dutycycle at 600Hz
     //first argument is pin 9
     //second argument is pin 10
-    PWM_set(10,0);
-    MOTOR_set_CCW();
+    //PWM_set(10,0);
+    //MOTOR_set_CW();
+
+    //read pin value, write the value back to serial
+    //sprintf(buf,"pin11: %d\r\n",readBit(PIND,2)); //read the value from pin 11, write to serial
+    //USART_putstring(buf);
+    //_delay_ms(1000);
   }
 
 }
--- pins.h
+++ pins.h
@@ -5,6 +5,8 @@
 //taken from http://www.electro-tech-online.com/threads/avr-gcc-any-comfortable-way-of-controlling-single-io-pins.121409/
 #define readBit(b,n) ((b) & (1<<(n)))
 //from http://www.electro-tech-online.com/threads/defining-and-using-bit-flags-in-c.140777/
+//
+#define setLED(x) setPin(PORTB,5,x)
 
 //define gate switching pin macros
 void PINS_init(void);
