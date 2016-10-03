7920697b553c818d3aee91ed2c853558b3c1d3ac
Patrick Savill <savillpatr@brer-fox.ecs.vuw.ac.nz>
2016-10-03 14:54:15 +1300

    Clean out master branch for arduino merge 	#deleted:    blink.test 	#deleted:    controller.c 	#deleted:    global.h 	#deleted:    makefile 	#deleted:    notes/initial_show_fuses_output.txt 	#deleted:    notes/notes.txt 	#deleted:    pins.c 	#deleted:    pins.h 	#deleted:    pwm.c 	#deleted:    pwm.h 	#deleted:    timers.c 	#deleted:    timers.h 	#deleted:    toolchain/README.txt 	#deleted:    toolchain/makefile 	#deleted:    uart.c 	deleted:    uart.h

--- pwm.h
+++
@@ -1,9 +0,0 @@
-#ifndef PWM_H
-#define PWM_H
-
-#define PWM_calculate_timer1_ovf(cpu_freq, pwm_freq, resolution) (1/(pwm_freq*resolution))/(1/cpu_freq)
-
-int PWM_set_half_pwm(int current, int pwm, int pwm_num);
-int PWM_set_full_pwm(int current, int pwm, int pwm_num);
-
-#endif
