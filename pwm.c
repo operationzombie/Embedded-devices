aca69f4ba3fdcc3c8ac4713a52fcd9528624fc1e
Layne Small <laynecsmall@gmail.com>
2016-10-19 12:43:03 +1300

    modified for new motordriver

--- pwm.c
+++ pwm.c
@@ -1,21 +1,19 @@
 #include "global.h"
+#include "pins.h"
 
 //values are 0-25 for dutycycle at 600Hz
 //with 0 being nothing, and 25 being 100%
 //first argument is pin 9
 //second argument is pin 10
-void PWM_set(int duty_1, int duty_2){
+void PWM_set(int duty_1){
   DDRB |= (1 << DDB1)|(1 << DDB2);
 	// PB1 and PB2 is now an output
 
 	ICR1 = 25;
 	// set TOP to 180
 
+	//specify dutycycle
 	OCR1A = duty_1;
-	// duty 177
-
-	OCR1B = duty_2;
-	// duty 177
 	
 	TCCR1A |= (1 << COM1A1)|(1 << COM1B1);
 	// set none-inverting mode
@@ -26,4 +24,10 @@ void PWM_set(int duty_1, int duty_2){
 	
 	TCCR1B |= (1 << CS12);
 	// 256 presc}
+}
+
+void PWM_disable(){
+	//TODO: unset COM1A1 COM1A0 bits
+	setPin(TCCR1A, 7, 0);
+	setPin(TCCR1A, 6, 0);
 }
--- pwm.h
+++ pwm.h
@@ -1,5 +1,5 @@
 #ifndef PWM_H
 #define PWM_H
-void PWM_set(int duty_1, int duty_2);
-
+void PWM_set(int duty_1);
+void PWM_disable();
 #endif
